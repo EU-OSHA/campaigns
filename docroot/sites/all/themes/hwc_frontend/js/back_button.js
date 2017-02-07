a2365939402d2c38c83f948e1c202e41d1726b96
dragos-dumi <dumitrescu.dragos1@gmail.com>
2017-02-07 19:12:56 +0200

    MDR-831 - back to link keep filters

--- docroot/sites/all/themes/hwc_frontend/js/back_button.js
+++ docroot/sites/all/themes/hwc_frontend/js/back_button.js
@@ -1,14 +1,18 @@
-(function ($, Drupal) {
-    Drupal.behaviors.skbp_back_button = {
-        attach: function (context, settings) {
-            $("body").once('skbp_back_button').each(function(){
-                if (document.referrer.indexOf(window.location.origin + '/search') === 0) {
-                    $(this).find('a.back-to-link').show().attr('href', document.referrer);
-                }
-                else {
-                    $(this).find('a.back-to-link').hide();
+(function($){
+    Drupal.behaviors.osha_back_button = {
+        attach: function(context, settings) {
+            $(".back-to-link").once('osha_back_button', function(){
+                var $this = $(this);
+                var url = window.location.origin + $this.attr('href');
+                // if user is coming from the listing, make history back
+                // to keep the potential query parameters.
+                if (document.referrer.indexOf(url) === 0) {
+                    $this.on('click', function(e){
+                        e.preventDefault();
+                        history.go(-1);
+                    });
                 }
             });
         }
-    };
-})(jQuery, Drupal);
+    }
+})(jQuery);
